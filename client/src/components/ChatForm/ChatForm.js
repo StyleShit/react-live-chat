29883715da6d77d7d0def41c40e8cbed90ce9d56
Evyatar Daud <evyatar.daud@gmail.com>
2020-11-02 21:30:10 +0200

    Client / Cleanup

--- client/src/components/ChatForm/ChatForm.js
+++ client/src/components/ChatForm/ChatForm.js
@@ -15,26 +15,26 @@ function ChatForm()
     
     // handle form submission
     const handleSubmit = ( e ) => {
-        
+
         e.preventDefault();
-        
+
         // prevent empty message
         if( !message )
             return;
-        
+
         // send message to the server
         socket.emit( 'chat-message', message );
-        
+
         // reset the message and focus on the input
         setMessage( '' );
         inputRef.current.focus();
-        
+
     };
-    
-    
+
+
     // handle message input keyup
     const handleKeyUp = ( e ) => {
-        
+
         // don't count `enter` as typing
         if( e.keyCode === 13 )
             return;
@@ -45,56 +45,56 @@ function ChatForm()
             // tell the server that the user has started typing
             socket.emit( 'start-typing' );
         }
-        
+
         // user was typing till now
         else
         {
             // clear the typing timeout message
             clearTimeout( typeTimeout );
         }
-        
-        
+
+
         // wait till user stops typing and tell the server
         setTypeTimeout( setTimeout( () => { 
 
             socket.emit( 'stop-typing' ); 
             setTypeTimeout( null );
 
-        }, 500 ) );
-        
+        }, 500 ));
+
     }
-    
-    
+
+
     // format `typers` array to a user friendly string
     const formatTypers = () => {
-        
+
         // return only user names, without current user
         const typers = typing.reduce( ( res, u ) => {
-            
+
             if( u.id !== socket.id )
-            res.push( u.userName );
-            
+                res.push( u.userName );
+
             return res;
-            
+
         }, [] );
-        
-        
+
+
         // nobody is typing ( maybe except the current user )
         if( typers.length === 0 )
             return '';
-        
-        
+
+
         // determine if it's `is` or `are` and return
         const isAre = ( typers.length === 1 ) ? ' is ' : ' are ';
-        
+
         return typers.join( ', ' ) + isAre + 'typing...';
-        
+
     };
-    
+
 
     // onmount
     useEffect( () => {
-        
+
         // listen to room typers
         socket.on( 'room-typing', ( typing ) => {
             setTyping( typing );
@@ -102,7 +102,7 @@ function ChatForm()
 
         // eslint-disable-next-line
     }, [] );
-    
+
 
     return (
         <div className="chat-form-container">
